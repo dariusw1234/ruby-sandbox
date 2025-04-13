8ff885902923caf7658b12ed7fe9f921a2bfbb47
Darius Woods <dariuswoods1919@gmail.com>
2025-04-13 23:51:20 +0000

    initial commit

---
+++ howdy.rb
@@ -0,0 +1,5 @@
+
+require "active_support/all"
+puts "What's your name, huh? Think you can just waltz all up in here?!"
+name = gets.chomp
+puts "Ahhh, #{name.capitalize}. They told me you'd be coming today. Nice name, by the way. Whatever that means."
